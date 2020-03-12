e6e4a2b333f88cc0aed635dd2ac3d28c3b65c7e5
Dandush03 <d.laloush@outlook.com>
2020-03-12 15:54:23 -0400

    Adding Controllers

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,4 +2,9 @@ class SessionsController < ApplicationController
 	def new
 		@user	= User.new()
 	end
+
+	def create
+		@user = User.find_by_email(params[:email])
+		@user.authenticate(params[:password])
+	end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,12 @@
 class User < ApplicationRecord
-  has_secure_password
+	has_secure_password
+	before_create :token
+
+	private
+
+	def token
+		temp = SecureRandom.urlsafe_base64
+		temp = Digest::SHA1.hexdigest temp
+		temp
+	end
 end
