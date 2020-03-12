aa0b9a8c87304da53e2e8ba5032adf1f33cc246a
Dandush03 <d.laloush@outlook.com>
2020-03-12 17:58:13 -0400

    added some functions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+	protect_from_forgery with: :exception
+	def permanent_coockie
+		temp = SecureRandom.urlsafe_base64
+		temp = Digest::SHA1.hexdigest temp
+		@user.update(:token => temp)
+		cookies.permanent[:remember_token] = temp
+	end
+
+	def setter
+		if @user = User.find_by_token(cookies.permanent[:remember_token])
+			render html: 'Congrats' + @user.inspect.to_s
+		end
+	end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,17 +1,20 @@
 class SessionsController < ApplicationController
 	def new
+		retriver
 		@user	= User.new()
 	end
 
 	def create
 		@user = User.find_by_email(params[:user][:email])
 		if(@user && @user.authenticate(params[:user][:password]))
-			temp = SecureRandom.urlsafe_base64
-			temp = Digest::SHA1.hexdigest temp
-			@user.update(:token => temp)
-			render html: 'YES!' + temp.to_s
-		else render html: 'Hello' + params[:user].inspect.to_s
+			permanent_coockie
+			render html: 'YES!' + cookies.permanent[:remember_token].inspect.to_s
+		else render html: 'Hello'
 		end
+	end
+
+	def delete
+		cookies.permanent[:remember_token] = ""
 	end
 
 	private
